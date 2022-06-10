13a9d5900f5305f352002b8a87b2e3fb130d4b8f
tsubasa283paris <tsubasa283paris@gmail.com>
2022-06-10 20:14:27 +0900

    Adapt dotenv variable declaration

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from dotenv import load_dotenv
-
+import settings
 from source.tab_client import TABClient
 
 tcclient = TABClient()
+
+token = settings.TOKEN
+channelID = settings.CHID
+
+if token == "" or channelID == "":
+    raise ValueError(".env not set properly")
 
 tcclient.load_channel(channelID)
 tcclient.run(token)
